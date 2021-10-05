c476782213a2c8af9e9eccb22be37b21d43b0b8c
hyochan <dooboolab@gmail.com>
2021-10-05 10:13:36 +0900

    More colors

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,44 @@
 <style>
   :global(body.light-mode) {
     --background: #fff;
-    --paper: #ededed;
+    --paper: #f2f5f6;
     --disabled: #c4c4c4;
     --text: #000;
+    --text-light: #1b1b1b;
     --textContrast: #fff;
     --placeholder: #6d6d6d;
+    --main: var(--primary);
+    --main-dark: var(--primary-light);
+    --card: #fff;
   }
   :global(body.dark-mode) {
     --background: #000;
-    --paper: #414141;
-    --disabled: #414141;
-    --text: #fff;
-    --textContrast: #000;
+    --paper: #2a2a2a;
+    --disabled: #515151;
     --placeholder: #6d6d6d;
+    --text: #fff;
+    --text-light: #d3d3d3;
+    --textContrast: #000;
+    --main: var(--primary-light);
+    --main-dark: var(--primary);
+    --card: #1c1c1c;
   }
   :global(body) {
+    --brand: #28db98;
     --primary: #0db293;
+    --primary-light: #75d0b8;
     --secondary: #00d9d5;
     --success: #33ff2f;
     --danger: #ff002e;
     --warning: #f2df0f;
     --info: #3a74e7;
+    --light: #ededed;
+    --red: #ff728d;
+    --orange: #f6a623;
+    --yellow: #eed100;
+    --green: #24cd97;
+    --blue: #679eff;
+    --purple: #b669f9;
 
     transition: background-color 0.3s;
     background-color: var(--background);
